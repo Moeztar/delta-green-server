16a69bc1bf71070ff9b1e43aa1a75d15aa80319c
Moeztar <40280000+Moeztar@users.noreply.github.com>
2021-05-24 13:19:38 -0400

    Update Agents.ts

--- src/Agents.ts
+++ src/Agents.ts
@@ -1,9 +1,9 @@
 export const Agents: Array<{user: string, password: string}> = [
   {user: 'newAgent', password: '***'},
-  {user: 'AnSi', password: 'coldPugilist'}
-  {user: 'DMercury', password: 'lawsHammer'}
-  {user: 'OtKr', password: 'klog2SQEP.loum4meef'}
-  {user: 'Moss', password: '11032'}
+  {user: 'AnSi', password: 'coldPugilist'},
+  {user: 'DMercury', password: 'lawsHammer'},
+  {user: 'OtKr', password: 'klog2SQEP.loum4meef'},
+  {user: 'Moss', password: '11032'},
   {user: 'aWhite', password: 'summerSn0w'}
 ]
 export default Agents;
